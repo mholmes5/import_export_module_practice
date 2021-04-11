dc3e29e463fdb120192132fb2d810999f513d197
Michael Holmes <michaelholmes618@gmail.com>
2021-04-11 13:17:40 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <ul>
+    <li>4</li>
+    <li>5</li>
+    <li>6</li>
+  </ul>,
+  document.getElementById("root")
+);
